dd51e8b87b2893936f3981a336f1a2c3eb9c13ba
ClaytonDewey <clay@dryadmedia.com>
2025-08-09 12:23:28 -0500

    chore: Add & style Frontend mentor attribution

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,14 @@
 export const Footer = () => {
   return (
     <footer className='footer__main'>
-      <p>Drag and drop to reorder list</p>
+      {/* <p>Drag and drop to reorder list</p> */}
+      <div class='attribution'>
+        Challenge by{' '}
+        <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+          Frontend Mentor
+        </a>
+        . Coded by <a href='https://www.claytondewey.com'>Clayton Dewey</a>.
+      </div>
     </footer>
   );
 };
