369a83c1b9cd9f25a73fe291df277cb2c5c0c076
ClaytonDewey <clay@dryadmedia.com>
2025-08-09 12:05:40 -0500

    create: Set up a password strength indicator

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,6 +3,7 @@ export { Button } from './Button';
 export { Footer } from './Footer';
 export { Header } from './Header';
 export { Input } from './Input';
+export { PasswordStrengthMeter } from './PasswordStrengthMeter';
 export { TodoFooter } from './TodoFooter';
 export { TodoItem } from './TodoItem';
 export { TodoList } from './TodoList';
