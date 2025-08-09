937d63901b6775a1338de0dbd6e81e8e551d4bf9
ClaytonDewey <clay@dryadmedia.com>
2025-08-09 15:15:46 -0500

    refactor: Make page h1 link to homepage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export const Header = ({ children }) => {
   return (
     <header className='todo__header'>
-      <h1>todo</h1>
+      <h1>
+        <Link to='/'>todo</Link>
+      </h1>
       {children}
     </header>
   );
