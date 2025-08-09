3a6cc49be0ac1a9801da99c7cb4f90b9d97170df
ClaytonDewey <clay@dryadmedia.com>
2025-08-09 14:12:23 -0500

    style: Arrange & style forgot password form

--- frontend/src/pages/ForgotPasswordPage.jsx
+++ frontend/src/pages/ForgotPasswordPage.jsx
@@ -1,3 +1,53 @@
+import { useState } from 'react';
+import { Button, Input } from '../components';
+import { Link } from 'react-router-dom';
+import { Mail, ArrowLeft } from 'lucide-react';
+
 export const ForgotPasswordPage = () => {
-  return <div>ForgotPasswordPage</div>;
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className='form__wrapper'>
+      <h2>Forgot Password</h2>
+      <form onSubmit={handleSubmit}>
+        <div className='form__message'>
+          <p className='text-center'>
+            Enter your email address and we'll send you a link to reset your
+            password.
+          </p>
+        </div>
+
+        <div className='form__group'>
+          <label htmlFor='email' className='sr-only'>
+            Email Address
+          </label>
+          <div className='form__icon-container'>
+            <Mail className='form__icon' />
+          </div>
+          <Input
+            // ref={inputRef}
+            type='email'
+            id='email'
+            placeholder='Email Address'
+            value={email}
+            autoComplete='email'
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <Button onSubmit={handleSubmit} type='submit' className='form__btn'>
+          Sign In
+        </Button>
+      </form>
+      <div className='form__footer text-center'>
+        <Link to={'/login'} className='form__footer-link'>
+          <ArrowLeft className='form__footer-icon' /> Back to Login
+        </Link>
+      </div>
+    </div>
+  );
 };
