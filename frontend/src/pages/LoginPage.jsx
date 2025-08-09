f309307df18de34d0ef243653d4fe1b8f3843a29
ClaytonDewey <clay@dryadmedia.com>
2025-08-09 13:19:51 -0500

    style: Set & style login form layout

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,3 +1,66 @@
+import { useState } from 'react';
+import { Button, Input } from '../components';
+import { Link } from 'react-router-dom';
+import { Lock, Mail } from 'lucide-react';
+
 export const LoginPage = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className='form__wrapper'>
+      <h2>Welcome Back</h2>
+      <form onSubmit={handleSubmit}>
+        <div className='form__group'>
+          <label htmlFor='email' className='sr-only'>
+            Email Address
+          </label>
+          <div className='form__icon-container'>
+            <Mail className='form__icon' />
+          </div>
+          <Input
+            // ref={inputRef}
+            type='email'
+            id='email'
+            placeholder='Email Address'
+            value={email}
+            autoComplete='email'
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className='form__group'>
+          <label htmlFor='password' className='sr-only'>
+            Password
+          </label>
+          <div className='form__icon-container'>
+            <Lock className='form__icon' />
+          </div>
+          <Input
+            // ref={inputRef}
+            type='password'
+            id='password'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div className='form__message'>
+          <p>
+            <Link to='/forgot-password' className='btn btn-text'>
+              Forgot password?
+            </Link>
+          </p>
+        </div>
+        <Button onSubmit={handleSubmit} type='submit' className='form__btn'>
+          Sign In
+        </Button>
+      </form>
+    </div>
+  );
 };
