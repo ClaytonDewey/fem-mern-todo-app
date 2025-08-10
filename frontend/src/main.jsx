c05444072c7eb59c3014ce34e447f549680cf679
ClaytonDewey <clay@dryadmedia.com>
2025-08-10 14:02:37 -0500

    refactor: Wrap app in tanstack query provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import queryClient from './config/queryClient.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <ReactQueryDevtools
+          buttonPosition='bottom-right'
+          initialIsOpen={false}
+        />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
