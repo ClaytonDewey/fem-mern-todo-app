c33edeb0093004e46cb29f68116abedcde400915
Clayton Dewey <clay@dryadmedia.com>
2025-08-10 18:18:41 -0500

    refactor: Add login functionality

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,19 +1,42 @@
 import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
 import { Button, Input } from '../components';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Lock, Mail } from 'lucide-react';
+import { login } from '../lib/api';
 
 export const LoginPage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const redirectUrl = location.state?.redirectUrl || '/';
+
+  const {
+    mutate: signIn,
+    // TODO: work on loading spinner
+    // isPending,
+    isError,
+  } = useMutation({
+    mutationFn: login,
+    onSuccess: () => {
+      navigate(redirectUrl, {
+        replace: true,
+      });
+    },
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    signIn({ email, password });
   };
 
   return (
     <div className='form__wrapper'>
-      <h2>Welcome Back</h2>
+      <h2>Sign in to your account</h2>
+      {isError && (
+        <div className='alert alert-danger'>Invalid email or password</div>
+      )}
       <form onSubmit={handleSubmit}>
         <div className='form__group'>
           <label htmlFor='email' className='sr-only'>
@@ -23,7 +46,6 @@ export const LoginPage = () => {
             <Mail className='form__icon' />
           </div>
           <Input
-            // ref={inputRef}
             type='email'
             id='email'
             placeholder='Email Address'
@@ -41,7 +63,6 @@ export const LoginPage = () => {
             <Lock className='form__icon' />
           </div>
           <Input
-            // ref={inputRef}
             type='password'
             id='password'
             placeholder='Password'
@@ -57,7 +78,11 @@ export const LoginPage = () => {
             </Link>
           </p>
         </div>
-        <Button onSubmit={handleSubmit} type='submit' className='form__btn'>
+        <Button
+          onSubmit={handleSubmit}
+          disabled={!email || password.length < 8}
+          type='submit'
+          className='form__btn'>
           Sign In
         </Button>
       </form>
