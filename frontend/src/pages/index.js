084f9e6399c55557a52b3caf73fd8e50185149ae
ClaytonDewey <clay@dryadmedia.com>
2025-08-10 16:10:27 -0500

    refactor: Use new email verification page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,3 +4,4 @@ export { HomePage } from './HomePage';
 export { LoginPage } from './LoginPage';
 export { ResetPasswordPage } from './ResetPasswordPage';
 export { SignUpPage } from './SignUpPage';
+export { VerifyEmail } from './VerifyEmail';
