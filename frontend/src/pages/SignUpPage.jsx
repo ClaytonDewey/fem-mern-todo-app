92d400e63f2e8fa6a37b664b491d4a9e130ab35d
ClaytonDewey <clay@dryadmedia.com>
2025-08-10 14:40:58 -0500

    refactor: Set up user registration submittal

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,20 +1,40 @@
 import { useState } from 'react';
 import { Button, Input, PasswordStrengthMeter } from '../components';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Lock, Mail, User } from 'lucide-react';
+import { register } from '../lib/api';
+import { useMutation } from '@tanstack/react-query';
 
 export const SignUpPage = () => {
+  const navigate = useNavigate();
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+
+  const {
+    mutate: createAccount,
+    isPending,
+    isError,
+    error,
+  } = useMutation({
+    mutationFn: register,
+    onSuccess: () => {
+      navigate('/', {
+        replace: true,
+      });
+    },
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    createAccount({ userName, email, password, confirmPassword });
   };
 
   return (
     <div className='form__wrapper'>
       <h2>Create an Account</h2>
+      {isError && <p>{error?.message || 'An error occured'}</p>}
       <form onSubmit={handleSubmit}>
         <div className='form__group'>
           <label htmlFor='name' className='sr-only'>
@@ -24,7 +44,6 @@ export const SignUpPage = () => {
             <User className='form__icon' />
           </div>
           <Input
-            // ref={inputRef}
             type='text'
             id='name'
             placeholder='Name'
@@ -42,7 +61,6 @@ export const SignUpPage = () => {
             <Mail className='form__icon' />
           </div>
           <Input
-            // ref={inputRef}
             type='email'
             id='email'
             placeholder='Email Address'
@@ -60,7 +78,6 @@ export const SignUpPage = () => {
             <Lock className='form__icon' />
           </div>
           <Input
-            // ref={inputRef}
             type='password'
             id='password'
             placeholder='Password'
@@ -79,7 +96,6 @@ export const SignUpPage = () => {
             <Lock className='form__icon' />
           </div>
           <Input
-            // ref={inputRef}
             type='password'
             id='confirmPassword'
             placeholder='Confirm Password'
@@ -88,8 +104,14 @@ export const SignUpPage = () => {
           />
         </div>
         {/* TODO: Show success indicator if password/confirmpassword match */}
-        <Button onSubmit={handleSubmit} type='submit' className='form__btn'>
-          Create Account
+        <Button
+          disabled={
+            !email || password.length < 8 || password !== confirmPassword
+          }
+          onSubmit={handleSubmit}
+          type='submit'
+          className='form__btn'>
+          {isPending ? 'Please wait...' : 'Create Account'}
         </Button>
 
         <footer className='form__footer'>
