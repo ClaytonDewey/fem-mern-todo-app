b6d8929266f10a838142d717c399e7226b5292dd
Clayton Dewey <clay@dryadmedia.com>
2025-08-12 11:41:32 -0500

    feat: Add Task update method

--- backend/src/controllers/task.controller.ts
+++ backend/src/controllers/task.controller.ts
@@ -1,7 +1,13 @@
 import catchErrors from '../utils/catchErrors';
 import { createTask } from '../services/task.service';
-import { BAD_REQUEST, CREATED, NOT_FOUND, OK } from '../constants/http';
-import TaskModel from '../models/task.model';
+import {
+  BAD_REQUEST,
+  CREATED,
+  FORBIDDEN,
+  NOT_FOUND,
+  OK,
+} from '../constants/http';
+import TaskModel, { TaskDocument } from '../models/task.model';
 import appAssert from '../utils/appAssert';
 
 export const createTaskHandler = catchErrors(async (req, res) => {
@@ -17,6 +23,36 @@ export const getTasksHandler = catchErrors(async (req, res) => {
   appAssert(tasks, NOT_FOUND, 'Todo items not found');
 
   res.status(OK).json(tasks);
+});
+
+interface UpdateTaskBody {
+  userId: string;
+  task?: string;
+  completed?: boolean;
+}
+
+export const updateTaskHandler = catchErrors(async (req, res) => {
+  const taskId = req.params.id;
+  const userId = req.userId;
+  const { task, completed } = req.body;
+
+  appAssert(userId, FORBIDDEN, 'Cannot edit this todo item');
+
+  const updateFields: Partial<Pick<TaskDocument, 'task' | 'completed'>> = {};
+  if (task !== undefined) updateFields.task = task;
+  if (completed !== undefined) updateFields.completed = completed;
+
+  const updatedTask = await TaskModel.findOneAndUpdate(
+    { _id: taskId, userId },
+    updateFields,
+    { new: true }
+  );
+  appAssert(
+    updatedTask,
+    NOT_FOUND,
+    'Todo item not found or user not authorized'
+  );
+  return res.status(OK).json(updatedTask);
 });
 
 export const deleteTaskHandler = catchErrors(async (req, res) => {
--- backend/src/routes/task.route.ts
+++ backend/src/routes/task.route.ts
@@ -3,6 +3,7 @@ import {
   createTaskHandler,
   deleteTaskHandler,
   getTasksHandler,
+  updateTaskHandler,
 } from '../controllers/task.controller';
 
 const taskRoutes = Router();
@@ -10,6 +11,7 @@ const taskRoutes = Router();
 // prefix: /tasks
 taskRoutes.post('/create-task', createTaskHandler);
 taskRoutes.get('/', getTasksHandler);
+taskRoutes.patch('/:id', updateTaskHandler);
 taskRoutes.delete('/:id', deleteTaskHandler);
 
 export default taskRoutes;
