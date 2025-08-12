6308009f37015bf4fa2df8bf6526b7765608ef36
Clayton Dewey <clay@dryadmedia.com>
2025-08-12 18:34:37 -0500

    feat: Add create task function

--- frontend/src/components/AddTodo.jsx
+++ frontend/src/components/AddTodo.jsx
@@ -1,38 +1,53 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Input } from '.';
 import toast from 'react-hot-toast';
+import { createTask } from '../lib/api';
 
 export const AddTodo = () => {
-  const [input, setInput] = useState('');
-  const inputRef = useRef(null);
+  const [task, setTask] = useState('');
+  const queryClient = useQueryClient();
+
+  const { mutate: createNewTask, isPending } = useMutation({
+    mutationFn: createTask,
+    onSuccess: () => {
+      toast.success('Todo item added successfully!');
+      queryClient.invalidateQueries({ queryKey: ['tasks'] }); // Refresh task list
+      setTask(''); // Clear input after success
+    },
+    onError: (error) => {
+      toast.error(error.message || 'Something went wrong');
+    },
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (input.trim() !== '') {
-      setInput('');
-      toast.success('Todo added successfully!');
-    } else {
-      toast.error('Todo field cannot be empty!');
+    if (task === '') {
+      toast.error('Please enter a todo item');
+      return;
     }
+    createNewTask({ task });
   };
 
   return (
-    <div className='todo todo__add'>
-      <div className='todo__toggle'></div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='todo' className='sr-only'>
-          New Todo
-        </label>
-        <Input
-          ref={inputRef}
-          type='text'
-          id='todo'
-          name='todo'
-          placeholder='Create a new todo...'
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-      </form>
-    </div>
+    <>
+      <div className='todo todo__add'>
+        <div className='todo__toggle'></div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='todo' className='sr-only'>
+            New Todo
+          </label>
+          <Input
+            type='text'
+            id='todo'
+            name='todo'
+            placeholder='Create a new todo...'
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            disabled={isPending}
+          />
+        </form>
+      </div>
+    </>
   );
 };
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -15,5 +15,7 @@ export const getSessions = async () => API.get('/sessions');
 export const deleteSession = async (id) => API.delete(`/sessions/${id}`);
 
 // Tasks
+export const createTask = async (data) => API.post('/tasks/create-task', data);
 export const getTasks = async () => API.get('/tasks');
+// export const updateTask = async (id) => API.patch(`/tasks/${id}`);
 export const deleteTask = async (id) => API.delete(`/tasks/${id}`);
